1523feb321e5019484b1fd251a06ce420e45cba3
Vanessa Barone <vanbarone.milani@gmail.com>
2022-02-17 17:28:41 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
@@ -72,6 +75,9 @@ public class DepartmentFormController implements Initializable{
 		catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 	}
 	
 	@FXML
@@ -102,17 +108,36 @@ public class DepartmentFormController implements Initializable{
 	public Department getFormData() {
 		Department obj = new Department();
 		
+		ValidationException exception = new ValidationException("Validation error");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtnome.getText() == null || txtnome.getText().trim().equals("")) {
+			exception.addError("name", "Field can´t be empty");
+		}
+		
 		obj.setName(txtnome.getText());
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
 	
 	private void notifyDataChangeListeners() {
-		
 		for (DataChangeListener listener : dataChangeListeners) {
 			listener.onDataChanged();
 		}
+	}
+	
+	private void setErrorMessages(Map<String,String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			lblErrorName.setText(errors.get("name"));
+		}
+		
 		
 	}
 	
